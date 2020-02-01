6af617ae6651d3d10c946c550171c9457d3a8a2a
111504 <35070932+111504@users.noreply.github.com>
2020-02-01 19:04:31 +0800

    delete

--- Algorithm/Project1/Header.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#define PI 3.1415926
-using namespace std;
-void swap(int &, int &);
-void MaxHeapify(vector<int>&, int, int);
-void BuildMaxHeap(vector<int> &);
-void HeapSort(vector<int> &);
-void PrintArray(vector<int> &);
